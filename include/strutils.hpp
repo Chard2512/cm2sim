1a423972045757e4de666daac2ca669b2ec04048
Chard2512 <chardson.coelho17@gmail.com>
2026-05-01 23:07:04 -0400

    Fixed unnecessary token after #endif

--- include/strutils.hpp
+++ include/strutils.hpp
@@ -8,4 +8,4 @@ std::vector<std::string> split(std::string text, const char sep);
 std::string substrFromChar(std::string text, const char sep);
 bool startsWith(std::string text, const std::string match);
 
-#endif strutils_hpp
+#endif
